484e9cca42518e665d3b9f4f16601733a7e37ac8
Levi Schwartzberg <Levischwartzberg@gmail.com>
2021-04-28 22:04:28 -0500

    Added boilerplate server file

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+// const Cardio = require("./cardioModel.js");
+// const Resistance = require("./resistanceModel.js");
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessdb", { useNewUrlParser: true });
+
+app.post("/submit", ({ body }, res) => {
+  Cardio.create(body)
+    .then(dbCardio => {
+      res.json(dbCardio);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+app.post("/submit", ({ body }, res) => {
+    Resistance.create(body)
+      .then(dbResistance => {
+        res.json(dbResistance);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
